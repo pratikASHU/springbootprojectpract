db79361c2848009199232b347a71d49c6f98bbaa
Pratik <pratik.kamble7@gmail.com>
2024-08-13 23:31:32 +0530

    Revert "39336-added user method in UserController.java class"
    This reverts commit 64a88ff6c12e1a5e65422271f24715c66d525fb2.

--- springbootprojectpractice/src/main/java/com/javatechpractice/springbootprojectpractice/controller/UserController.java
+++ springbootprojectpractice/src/main/java/com/javatechpractice/springbootprojectpractice/controller/UserController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-    @RequestMapping("/")
-    public String user() {
+    @RequestMapping("/profile/user")
+    public String profile() {
         System.out.println("This is home page");
-        return "user";
+        return "profile/user";
+    }
+    @RequestMapping("/profile/home")
+    public String home() {
+        System.out.println("This is home page");
+        return "profile/home";
+    }
+
+     @RequestMapping("/profile/details")
+    public String details() {
+        System.out.println("This is details page");
+        return "profile/details";
     }
 }
